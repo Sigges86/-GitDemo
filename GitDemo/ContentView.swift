e4efec1cf476dd6d1472a0c50dcc381c958b3ad3
Sigge <sigges.86@gmail.com>
2020-05-09 18:25:06 +0200

    Initial Commit

---
+++ GitDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GitDemo
+//
+//  Created by Sigge Örn on 2020-05-09.
+//  Copyright © 2020 Sigge Örn. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
